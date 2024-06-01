88278226f911c2dd2ee60aae5e142d720dca78cc
vagnermaltauro <vagnermaltauro@gmail.com>
2024-06-01 09:07:59 -0300

    feat: add create account functionality with query

--- storage.go
+++ storage.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+
 	_ "github.com/lib/pq"
 )
 
@@ -48,7 +50,25 @@ func (s *PostgresStore) createAccountTable() error {
 	return err
 }
 
-func (s *PostgresStore) CreateAccount(*Account) error {
+func (s *PostgresStore) CreateAccount(acc *Account) error {
+	query := `insert into account
+  (first_name, last_name, number, balance, created_at)
+  values($1, $2, $3, $4, $5)`
+
+	resp, err := s.db.Query(
+		query,
+		acc.FirstName,
+		acc.LastName,
+		acc.Number,
+		acc.Balance,
+		acc.CreatedAt)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%+v\n", resp)
+
 	return nil
 }
 
