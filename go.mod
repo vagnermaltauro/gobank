ae4591f879ad57efaeb59f3cd49be29605a1603c
vagnermaltauro <vagnermaltauro@gmail.com>
2024-07-30 20:02:17 -0300

    feat: add password encryption to NewAccount function

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/lib/pq v1.10.9
 )
 
-require github.com/golang-jwt/jwt/v5 v5.2.1
-
-require golang.org/x/crypto v0.25.0 // indirect
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.1
+	golang.org/x/crypto v0.25.0
+)
--- types.go
+++ types.go
@@ -3,6 +3,8 @@ package main
 import (
 	"math/rand"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type CreateAccountRequest struct {
@@ -30,11 +32,17 @@ type TransferRequest struct {
 	Amount    int `json:"amount"`
 }
 
-func NewAccount(firstName, lastName, password string) *Account {
-	return &Account{
-		FirstName: firstName,
-		LastName:  lastName,
-		Number:    int64(rand.Intn(1000000)),
-		CreatedAt: time.Now().UTC(),
+func NewAccount(firstName, lastName, password string) (*Account, error) {
+	encpw, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
 	}
+
+	return &Account{
+		FirstName:         firstName,
+		LastName:          lastName,
+		EncryptedPassword: string(encpw),
+		Number:            int64(rand.Intn(1000000)),
+		CreatedAt:         time.Now().UTC(),
+	}, nil
 }
